53ad23d1faae69fe1650946bf1519a40e7cf9069
Neero2000 <mensefgames@gmail.com>
2024-05-08 15:02:59 +0200

    moncef

--- lib/src/features/home/index.dart
+++ lib/src/features/home/index.dart
@@ -3,6 +3,7 @@ export 'pages/client_home_page.dart';
 export 'pages/notifications_page.dart';
 export 'pages/home_page.dart';
 export 'pages/appointments_page.dart';
+export 'pages/Searchdoctor.dart';
 
 // widgets
 export 'widgets/speciality_item.dart';
---
+++ lib/src/features/home/pages/Searchdoctor.dart
@@ -0,0 +1,2 @@
+import '../../../config/index.dart';
+//if u click to any category u will find the same doctors 
